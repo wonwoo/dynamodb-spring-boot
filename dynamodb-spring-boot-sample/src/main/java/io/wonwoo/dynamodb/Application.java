515566f6d79e3df7d61211ebcc41ccb920fd1a9f
wonwoo <aoruqjfu@gmail.com>
2017-12-04 22:32:35 +0900

    import remove

--- dynamodb-spring-boot-sample/src/main/java/io/wonwoo/dynamodb/Application.java
+++ dynamodb-spring-boot-sample/src/main/java/io/wonwoo/dynamodb/Application.java
@@ -1,9 +1,7 @@
 package io.wonwoo.dynamodb;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
